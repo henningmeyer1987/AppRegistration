585f82e28adfc77b71bc870874735d13b438edf5
Henning Meyer <h.meyer@affiliprint.de>
2015-03-16 11:45:07 +0100

    migration

--- bhg_django/report_types/models.py
+++ bhg_django/report_types/models.py
@@ -2,5 +2,4 @@ from django.db import models
 
 
 class report_types(models.Model):
-	description = models.CharField(max_length=200)
-	report_types = models.IntegerField(max_length=1)
+	type_name = models.CharField(max_length=200)
